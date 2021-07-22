9f2c8c56a86c1bd1a55b0b60e1b1d8929be5ca58
David Chen <davidc3287@gmail.com>
2021-07-22 17:07:31 +0000

    set innerHTML based on match

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -3,9 +3,13 @@ const getMessages = () => {
     const messagesRef = firebase.database().ref();
     messagesRef.on("value", (snapshot) => {
         const data = snapshot.val()
-        console.log(data)
         for (let key in data) {
             console.log(key, data[key])
+            if (key === passcode.value) {
+                console.log("match found");
+                const message = document.querySelector("#message")
+                message.innerHTML = data[key]
+            }
         }
     })
 }
