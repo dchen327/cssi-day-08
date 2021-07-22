1a2ff30c970bf851cc533d69f49318a08d38f7a4
David Chen <davidc3287@gmail.com>
2021-07-22 17:12:22 +0000

    show error message

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -3,13 +3,18 @@ const getMessages = () => {
     const messagesRef = firebase.database().ref();
     messagesRef.on("value", (snapshot) => {
         const data = snapshot.val()
+        let matchFound = false
         for (let key in data) {
             console.log(key, data[key])
             if (key === passcode.value) {
-                console.log("match found");
+                matchFound = true
                 const message = document.querySelector("#message")
                 message.innerHTML = data[key]
             }
+        }
+        if (!matchFound) {
+            const message = document.querySelector("#message")
+            message.innerHTML = "No match found!!"
         }
     })
 }
